cedbb821fd86285bd8076fd8087f43386838893e
Paul <paul@theapplegates.com>
2024-05-27 16:43:59 -0400

    pic test

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,34 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+// import eleventyWebcPlugin from "@11ty/eleventy-plugin-webc";
+import { eleventyImagePlugin } from "@11ty/eleventy-img";
+
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 export default function(eleventyConfig) {
+eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+	// which file extensions to process
+	extensions: "html",
+
+	// Add any other Image utility options here:
+
+	// optional, output image formats
+	formats: ["avif", "webp", "jpeg"],
+	// formats: ["auto"],
+
+	// optional, output image widths
+	 widths: [320, 570, 880, 1024, 1248],
+
+	// optional, attributes assigned on <img> override these values.
+	defaultAttributes: {
+		sizes: "90vw",
+		loading: "lazy",
+		decoding: "async",
+	},
+});
+
+
 	eleventyConfig.ignores.add("README.md");
 
 	eleventyConfig.addPlugin(pluginWebc, {
